bce97c0a523851b8ee7ec9fb8e98fa0c0439836b
Nikolay Z <nzaharov86@gmail.com>
2018-07-05 20:22:48 +0300

    altComm

--- src/App.js
+++ src/App.js
@@ -3,38 +3,63 @@ import TransactionArr from './transaction.js';
 import Btn from './Btn';
 
 
+  export function sorting  (sortParam){ 
+  switch(sortParam){
+    case sortParam.inc==true:
+    this.state.items.filter(w => w.type === "income")
+    break;
+    case sortParam.cons==true:
+    this.state.items.filter(w => w.type === "consumption")
+    break;
+    case sortParam.val==true:
+    this.state.items.filter(w => w.value >1000 )
+    break;
+    case sortParam.date==true:
+    this.state.items.filter(w => w.dt.getMonth() === new Date().getMonth()) 
+    break;
+  }
+};
+
+ 
+
+
 class App extends Component {
+
+
+ 
 
   constructor(props)
   {
    super(props);
-   this.state = {items:TransactionArr }
-   this.initialState = {items:TransactionArr}
+   this.state = {items:TransactionArr, inc: false, val: false, cons: false, date: false}
+   this.obj = {inc: false, val: false, cons: false, date: false}
   }
-
-  
-  resetsortVal =          () => this.setState({items:  this.state.items.filter(w => w.value >1000 )})
-  sortVal  =              () => this.setState({items:  this.state.items.filter(w => w.value < 1000 )})  
-  sortTypeByIncome =      () => this.setState({items:  this.state.items.filter(w => w.type === "income") })
-  sortTypeByConsumption = () => this.setState({items:  this.state.items.filter(w => w.type === "consumption") })
-  sortDate =              () => this.setState({items:  this.state.items.filter(w => w.dt.getMonth() === new Date().getMonth()) })
-
-
-
   render() { 
-    let baseState = this.state;
+  switcher = (ob) =>{
+      if (ob.inc == true) ob.inc = true 
+        else this.state.inc= false
+      if (ob.val == true) ob.val = true  
+        else this.state.val=  false 
+      if (ob.cons == true) ob.cons = true 
+        else this.state.cons=  false 
+      if (ob.date == true) ob.date =true 
+        else this.state.date= false 
+      sorting(ob);
+    }
     return (
+      
       <div className="App">
+
         <table border = "1" cellSpacing="0" >
           <tbody>
           <tr>
           <td></td>
-          <td><Btn sortType ="value" toDoMeth={  this.sortVal }              btnname={"value"} /></td>
+          <td><Btn sortType ="value" toDoMeth={ this.switcher(this.state) }  btnname={"value"} /></td>
           <td>
-              <Btn sortType ="type"  toDoMeth={ this.sortTypeByIncome}       btnname={"sortTypeByIncome"} />
-              <Btn sortType ="type"  toDoMeth={ this.sortTypeByConsumption}  btnname={"sortTypeByConsumption"} />
+              <Btn sortType ="type"  toDoMeth={this.switcher(this.state) }   btnname={"sortTypeByIncome"} />
+              <Btn sortType ="type"  toDoMeth={ this.switcher(this.state) }  btnname={"sortTypeByConsumption"} />
           </td>
-          <td><Btn sortType ="date"  toDoMeth={ this.sortDate}               btnname={"date"} /></td>
+          <td><Btn sortType ="date"  toDoMeth={this.switcher(this.state) }  btnname={"date"} /></td>
           
           </tr>
           
@@ -49,7 +74,50 @@ class App extends Component {
           </table>
       </div>
     )
-  }     
-
+  } 
 };
 export default App;
+
+
+/* resetsortVal =          () => this.setState({items:  this.state.items.filter(w => w.value >1000 )})
+   
+  sortTypeByIncome =      () => this.setState({items:  this.state.items.filter(w => w.type === "income") })
+  sortTypeByConsumption = () => this.setState({items:  this.state.items.filter(w => w.type === "consumption") })
+  sortDate =              () => this.setState({items:  this.state.items.filter(w => w.dt.getMonth() === new Date().getMonth()) })
+
+*/
+
+/*  valSort = () => this.obj.val=true
+  incSort = () => this.obj.inc=true
+  consSort= () => this.obj.cons=true
+  dateSort= () => this.obj.date=true
+*/
+
+  
+  
+  
+
+
+    /*
+    if (sortParam === "val")
+    {
+      // this.setState({items:  this.state.items.filter(w => w.value >1000 )})
+    }
+  
+    if (sortParam==="inc")
+    {
+       this.state.inc=true
+    }
+  
+    if(sortParam ==="cons")
+    {
+       this.state.cons=true
+    }
+    
+    if(sortParam === "date")
+    {
+       this.state.date=true
+    }   
+    */
+  
+
